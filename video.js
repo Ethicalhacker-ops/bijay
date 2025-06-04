27f7e43c60d9046fa154ef2ad12a6e5c6a03a768
The Bijay <138951312+Ethicalhacker-ops@users.noreply.github.com>
2025-06-04 10:29:01 +0545

    Create video.js

---
+++ video.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const VideoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    instructor: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    },
+    videoUrl: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Video', VideoSchema);
