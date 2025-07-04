505b4086fd40bfcfa939d9cd217c5eb320709a36
The Bijay <138951312+Ethicalhacker-ops@users.noreply.github.com>
2025-07-04 09:33:35 +0545

    Create vite.config.ts

---
+++ public/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        404: './public/404.html'
+      }
+    }
+  }
+});
